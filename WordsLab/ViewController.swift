0d9636c4578ab34649eea488fdc1e164834f5cd0
Sergey Belousov <seryozha.belousov@gmail.com>
2023-01-30 18:23:41 +0200

    Initial Commit

---
+++ WordsLab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordsLab
+//
+//  Created by Сергей Белоусов on 30.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
