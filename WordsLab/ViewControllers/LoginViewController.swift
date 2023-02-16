289a4f943bf03b5cea39e57970dd71410a603891
Sergey Belousov <seryozha.belousov@gmail.com>
2023-02-16 20:19:57 +0200

    code refactoring

---
+++ WordsLab/ViewControllers/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  WordsLab
+//
+//  Created by Сергей Белоусов on 03.02.2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func loadView() {
+        self.view = LoginView()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
