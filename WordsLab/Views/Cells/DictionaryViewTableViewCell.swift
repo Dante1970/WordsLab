20992fc0cef9c147c81c73428ce516cbde1bcf30
Sergey Belousov <seryozha.belousov@gmail.com>
2023-04-17 22:01:00 +0300

    add delete button in DictionaryViewTableViewCell

--- WordsLab/Views/Cells/DictionaryViewTableViewCell.swift
+++ WordsLab/Views/Cells/DictionaryViewTableViewCell.swift
@@ -11,6 +11,13 @@ class DictionaryViewTableViewCell: UITableViewCell {
 
     static let identifier = "DictionaryViewTableViewCell"
     
+    var mainViewTopAnchor: NSLayoutConstraint?
+    var mainViewHeightAnchor: NSLayoutConstraint?
+    var mainViewLeadingAnchor: NSLayoutConstraint?
+    var mainViewTrailingAnchor: NSLayoutConstraint?
+    
+    var deleteButtonWidth: NSLayoutConstraint?
+    
     var viewModel: DictionaryTableViewCellViewModel? {
         willSet(viewModel) {
             guard let viewModel = viewModel else { return }
@@ -21,7 +28,7 @@ class DictionaryViewTableViewCell: UITableViewCell {
         }
     }
     
-    private let mainView: UIView = {
+    let mainView: UIView = {
         let view = UIView()
         view.backgroundColor = BaseColors.backgroundForBlocks
         view.layer.cornerRadius = 20
@@ -66,6 +73,17 @@ class DictionaryViewTableViewCell: UITableViewCell {
         return imageView
     }()
     
+    private let deleteButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Delete", for: .normal)
+        button.backgroundColor = .red
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.clipsToBounds = true
+        button.addTarget(self, action: #selector(deleteButtonTapped), for: .touchUpInside)
+        button.layer.cornerRadius = 20
+        return button
+    }()
+    
     // MARK: - layoutSubviews
     override func layoutSubviews() {
         super.layoutSubviews()
@@ -83,18 +101,87 @@ class DictionaryViewTableViewCell: UITableViewCell {
         
         makeUI()
         setupConstraints()
+        addGesture()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - addGesture
+    private func addGesture() {
+        
+        let swipeGestureLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeGesture(_:)))
+        swipeGestureLeft.direction = .left
+        mainView.addGestureRecognizer(swipeGestureLeft)
+        
+        let swipeGestureRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeGesture(_:)))
+        swipeGestureRight.direction = .right
+        mainView.addGestureRecognizer(swipeGestureRight)
+    }
+    
+    @objc private func deleteButtonTapped() {
+        deleteAnimation()
+    }
+    
+    // MARK: - Animations
+    private func deleteAnimation() {
+        
+        UIView.animate(withDuration: 0.3, delay: 0.0, options: .curveEaseIn, animations: { [weak self] in
+            
+            guard let self = self else { return }
+            
+            self.mainViewTopAnchor!.constant = 1
+            self.mainViewHeightAnchor!.constant = 0
+            self.mainViewLeadingAnchor!.constant -= 300
+            self.mainViewTrailingAnchor!.constant -= 300
+            
+            self.deleteButtonWidth!.constant += 300
+            
+            self.layoutIfNeeded()
+            
+            guard let tableView = self.superview as? UITableView else { return }
+            tableView.beginUpdates()
+            tableView.endUpdates()
+            
+        }, completion: nil)
+    }
+    
+    @objc private func handleSwipeGesture(_ gesture: UISwipeGestureRecognizer) {
+
+        let indentForMainView: CGFloat = 90
+        let deleteButtonWidth: CGFloat = 50
+        let leadingConstant = self.mainViewLeadingAnchor!.constant
+        
+        UIView.animate(withDuration: 0.3, animations: { [weak self] in
+            
+            guard let self = self else { return }
+                
+            if gesture.direction == .left && leadingConstant == 20 {
+                
+                self.mainViewLeadingAnchor!.constant -= indentForMainView
+                self.mainViewTrailingAnchor!.constant -= indentForMainView
+                
+                self.deleteButtonWidth!.constant += deleteButtonWidth
+                
+            } else if gesture.direction == .right && leadingConstant < 20 {
+                
+                self.mainViewLeadingAnchor!.constant += indentForMainView
+                self.mainViewTrailingAnchor!.constant += indentForMainView
+                
+                self.deleteButtonWidth!.constant -= deleteButtonWidth
+            }
+                
+            self.layoutIfNeeded()
+        }, completion: nil)
+    }
     
     // MARK: - makeUI
     private func makeUI() {
         
         contentView.backgroundColor = BaseColors.backgroundColor
         
+        contentView.addSubview(deleteButton)
         contentView.addSubview(mainView)
         mainView.addSubview(folderView)
         folderView.addSubview(folderLabel)
@@ -107,6 +194,7 @@ class DictionaryViewTableViewCell: UITableViewCell {
     private func setupConstraints() {
         
         mainView.translatesAutoresizingMaskIntoConstraints = false
+        deleteButton.translatesAutoresizingMaskIntoConstraints = false
         folderView.translatesAutoresizingMaskIntoConstraints = false
         folderLabel.translatesAutoresizingMaskIntoConstraints = false
         dictionaryName.translatesAutoresizingMaskIntoConstraints = false
@@ -114,12 +202,26 @@ class DictionaryViewTableViewCell: UITableViewCell {
         dictionaryImageView.translatesAutoresizingMaskIntoConstraints = false
         
         let padding: CGFloat = 20
+        
+        mainViewTopAnchor = mainView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20)
+        mainViewHeightAnchor = mainView.heightAnchor.constraint(equalToConstant: 85)
+        mainViewLeadingAnchor = mainView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding)
+        mainViewTrailingAnchor = mainView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding)
+        
+        deleteButtonWidth = deleteButton.widthAnchor.constraint(equalToConstant: 30)
+        
         NSLayoutConstraint.activate([
-            mainView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
-            mainView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
-            mainView.heightAnchor.constraint(equalToConstant: 85),
-            mainView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            
+            mainViewTopAnchor!,
+            mainViewLeadingAnchor!,
+            mainViewHeightAnchor!,
+            mainViewTrailingAnchor!,
             mainView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            
+            deleteButton.topAnchor.constraint(equalTo: mainView.topAnchor),
+            deleteButtonWidth!,
+            deleteButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            deleteButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
             
             folderView.topAnchor.constraint(equalTo: mainView.topAnchor, constant: 10),
             folderView.leadingAnchor.constraint(equalTo: mainView.leadingAnchor, constant: padding),
